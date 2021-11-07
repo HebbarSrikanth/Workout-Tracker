b88786892c3f43052b20eca3ba1820378781f5f7
hebbarsrikanth <srikanth.hebbar14@outlook.com>
2021-11-07 13:26:08 +0530

    Added route-title & route-guard

--- src/router/index.js
+++ src/router/index.js
@@ -4,32 +4,48 @@ import Register from "../views/Register.vue";
 import Login from "../views/Login.vue";
 import ViewWorkot from "../views/ViewWorkout.vue";
 import Create from "../views/Create.vue";
+import { supabase } from "../supabase";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      auth: false,
+    },
   },
   {
     path: "/register",
     name: "Singup",
     component: Register,
+    meta: {
+      auth: false,
+    },
   },
   {
     path: "/login",
     name: "LogIn",
     component: Login,
+    meta: {
+      auth: false,
+    },
   },
   {
     path: "/view/:workoutId?",
     name: "View",
     component: ViewWorkot,
+    meta: {
+      auth: false,
+    },
   },
   {
     path: "/create",
     name: "create",
     component: Create,
+    meta: {
+      auth: true,
+    },
   },
 ];
 
@@ -45,5 +61,18 @@ router.beforeEach((to, from, next) => {
 });
 
 // Route guard for auth routes
+
+router.beforeEach((to, from, next) => {
+  const user = supabase.auth.user();
+  if (to.matched.some((res) => res.meta.auth)) {
+    if (user) {
+      next();
+      return;
+    } else {
+      router.push("/login");
+    }
+  }
+  next();
+});
 
 export default router;
